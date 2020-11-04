03cd953ae96271a8501c68320571f2e3e33a3cdb
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-04 19:50:39 +0100

    wip

--- users/user-router.js
+++ users/user-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const User = require('./user-model');
 const db = require("../data/db-config.js");
 
 const router = express.Router();
@@ -33,6 +33,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// get all posts by a particular user id
 router.get('/:id/posts', (req, res) => {
   User.findPosts()
 })
